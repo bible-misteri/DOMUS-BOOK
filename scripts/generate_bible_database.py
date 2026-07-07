348e465400fb4b862a181cc6f5ba5a871f6ee16e
bible-misteri <sandhisuito@gmail.com>
2026-07-07 21:49:32 +0700

    Create generate_bible_database.py

---
+++ scripts/generate_bible_database.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+print("==================================")
+print("DOMUS Bible Database Generator")
+print("==================================")
+print()
+print("Generating bible_database.yaml ...")
