123c258d015753af58e818be2b74d323e3ca51c7
bible-misteri <sandhisuito@gmail.com>
2026-07-11 22:04:27 +0700

    Update validation.py

--- domus/validation.py
+++ domus/validation.py
@@ -1,1 +1,22 @@
+# ----------------------------------
+# DOMUS Validation
+# ----------------------------------
 
+def validate_books(metadata):
+
+    if "books" not in metadata:
+
+        raise Exception(
+            "Missing books section."
+        )
+
+    if not isinstance(
+        metadata["books"],
+        list
+    ):
+
+        raise Exception(
+            "Books must be a list."
+        )
+
+    return True
