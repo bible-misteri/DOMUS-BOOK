188ee7783aefa1dd8f04bbfb59a2379dfd876950
bible-misteri <sandhisuito@gmail.com>
2026-07-11 23:58:26 +0700

    Update test_scanner.py

--- tests/test_scanner.py
+++ tests/test_scanner.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python3
 
+from domus.scanner import scan
+
+text = """
+Yoh 3:16
+Mazmur 23
+1 Kor 13:4-7
+Kejadian 1:1
+"""
+
+results = scan(text)
+
+for item in results:
+    print(item)
