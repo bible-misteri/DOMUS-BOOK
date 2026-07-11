a477f06ef16959b3c583a364003989d91ee34d73
bible-misteri <sandhisuito@gmail.com>
2026-07-11 22:05:18 +0700

    Update bible.py

--- domus/bible.py
+++ domus/bible.py
@@ -1,1 +1,35 @@
+# ----------------------------------
+# DOMUS Bible Utilities
+# ----------------------------------
 
+def count_books(metadata):
+
+    return len(
+        metadata["books"]
+    )
+
+
+def count_chapters(metadata):
+
+    total = 0
+
+    for book in metadata["books"]:
+
+        total += len(
+            book["chapters"]
+        )
+
+    return total
+
+
+def count_verses(metadata):
+
+    total = 0
+
+    for book in metadata["books"]:
+
+        total += sum(
+            book["chapters"]
+        )
+
+    return total
