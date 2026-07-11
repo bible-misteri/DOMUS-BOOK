e7f15b4588cde2a36b624cbdeb6ab0659bbd10d4
bible-misteri <sandhisuito@gmail.com>
2026-07-11 18:22:33 +0700

    Update filesystem.py

--- domus/filesystem.py
+++ domus/filesystem.py
@@ -1,1 +1,13 @@
+from pathlib import Path
 
+repo_root = Path(__file__).resolve().parent.parent
+
+
+def ensure_folder(folder):
+
+    folder.mkdir(
+        parents=True,
+        exist_ok=True
+    )
+
+    return folder
