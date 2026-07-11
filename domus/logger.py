10a3183b3df7485c5beccfe59192ae10a695ce8f
bible-misteri <sandhisuito@gmail.com>
2026-07-11 18:23:10 +0700

    Update logger.py

--- domus/logger.py
+++ domus/logger.py
@@ -1,1 +1,16 @@
+def header(title):
 
+    print("==================================")
+    print(title)
+    print("==================================")
+    print()
+
+
+def success(message):
+
+    print(f"✓ {message}")
+
+
+def info(message):
+
+    print(message)
