24a9cd424d0e692decb677c46634426966408fc5
bible-misteri <sandhisuito@gmail.com>
2026-07-12 22:21:45 +0700

    Update test_validator.py

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -1,1 +1,75 @@
+from domus.validation import validate
 
+database = {
+
+    "Yohanes": {
+
+        "chapters": {
+
+            1: 51,
+
+            2: 25,
+
+            3: 36
+
+        }
+
+    }
+
+}
+
+assert validate(
+
+    {
+
+        "book": "Yohanes",
+
+        "chapter": 3,
+
+        "verse": 16,
+
+        "verse_end": None
+
+    },
+
+    database
+
+)
+
+print("✓ Validator")
+
+assert not validate(
+
+    {
+
+        "book": "Yohanes",
+
+        "chapter": 99,
+
+        "verse": 1,
+
+        "verse_end": None
+
+    },
+
+    database
+
+)
+
+assert not validate(
+
+    {
+
+        "book": "Yohanes",
+
+        "chapter": 3,
+
+        "verse": 99,
+
+        "verse_end": None
+
+    },
+
+    database
+
+)
