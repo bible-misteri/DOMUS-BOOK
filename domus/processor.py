b21ab05443fa4077c5d4dc0e7d096da517e408e7
bible-misteri <sandhisuito@gmail.com>
2026-07-12 22:58:31 +0700

    Update processor.py

--- domus/processor.py
+++ domus/processor.py
@@ -1,1 +1,17 @@
+# ----------------------------------
+# DOMUS Markdown Processor
+# ----------------------------------
 
+from domus.engine import BibleEngine
+
+
+class MarkdownProcessor:
+
+    def __init__(self, database):
+
+        self.engine = BibleEngine(database)
+
+
+    def process(self, text):
+
+        return self.engine.parse_text(text)
