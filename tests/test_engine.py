9b0a505407fcd5a7c7cb1fc140c60c3b349ec13a
bible-misteri <sandhisuito@gmail.com>
2026-07-12 22:41:53 +0700

    Update test_engine.py

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,1 +1,39 @@
+from domus.engine import BibleEngine
 
+database = {
+
+    "Yohanes": {
+
+        "chapters": {
+
+            1: 51,
+            2: 25,
+            3: 36
+
+        }
+
+    }
+
+}
+
+engine = BibleEngine(database)
+
+results = engine.parse_text(
+
+    "Yoh 3:16"
+
+)
+
+print(results)
+
+[
+    {
+        "raw":"Yoh 3:16",
+        "book":"Yohanes",
+        "chapter":3,
+        "verse":16,
+        "verse_end":None,
+        "type":"single",
+        "valid":True
+    }
+]
