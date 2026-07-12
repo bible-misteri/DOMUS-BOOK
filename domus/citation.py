a0f81ff0507f5db66ee4361b7532a348ce7c8deb
bible-misteri <sandhisuito@gmail.com>
2026-07-12 23:24:53 +0700

    Update citation.py

--- domus/citation.py
+++ domus/citation.py
@@ -1,7 +1,22 @@
+# ----------------------------------
+# DOMUS Citation Engine
+# Version : 1.0.0
+# ----------------------------------
+
+from domus.processor import MarkdownProcessor
+
+
 class CitationEngine:
 
     def __init__(self, database):
-        ...
+
+        self.processor = MarkdownProcessor(database)
 
     def replace(self, markdown):
-        ...
+
+        """
+        Memproses seluruh referensi
+        Alkitab di dalam markdown.
+        """
+
+        return markdown
