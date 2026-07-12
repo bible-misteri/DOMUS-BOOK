6334593281be8df320815ac7ab65cb8331b9a006
bible-misteri <sandhisuito@gmail.com>
2026-07-12 22:36:34 +0700

    Create engine.py

---
+++ domus/engine.py
@@ -0,0 +1,42 @@
+# ----------------------------------
+# DOMUS Bible Engine
+# ----------------------------------
+
+from domus.scanner import scan
+from domus.parser import parse
+from domus.resolver import (
+    load_aliases,
+    resolve
+)
+from domus.validation import validate
+
+
+class BibleEngine:
+
+    def __init__(self, database):
+
+        self.database = database
+
+        self.aliases = load_aliases()
+
+    def parse_text(self, text):
+
+        references = []
+
+        for item in scan(text):
+
+            ref = parse(item)
+
+            ref = resolve(
+                ref,
+                self.aliases
+            )
+
+            ref["valid"] = validate(
+                ref,
+                self.database
+            )
+
+            references.append(ref)
+
+        return references
