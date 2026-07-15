ea0876c11129009a40d824f808c17d119477833d
bible-misteri <sandhisuito@gmail.com>
2026-07-15 14:04:01 +0700

    Create indexer.py

---
+++ domus/indexer.py
@@ -0,0 +1,26 @@
+# ----------------------------------
+# DOMUS Bible Index
+# ----------------------------------
+
+from collections import defaultdict
+
+
+def build_index(references):
+
+    index = defaultdict(list)
+
+    for ref in references:
+
+        verse = str(ref.chapter)
+
+        if ref.verse is not None:
+
+            verse += f":{ref.verse}"
+
+            if ref.verse_end:
+
+                verse += f"-{ref.verse_end}"
+
+        index[ref.book].append(verse)
+
+    return dict(index)
