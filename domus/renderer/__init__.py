8561bf7dfcef6a8f185f48327b2d8c61b9200fa7
bible-misteri <sandhisuito@gmail.com>
2026-07-15 23:26:32 +0700

    Update and rename domus/renderer.py to domus/renderer/__init__.py

---
+++ domus/renderer/__init__.py
@@ -0,0 +1,8 @@
+# ----------------------------------
+# DOMUS Renderer Engine
+# ----------------------------------
+
+from .html import HTMLRenderer
+from .latex import LaTeXRenderer
+from .epub import EPUBRenderer
+from .docx import DOCXRenderer
