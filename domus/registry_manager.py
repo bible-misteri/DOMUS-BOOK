d5558ff58aecf9dd5bc75c3dd687e81203caec7b
bible-misteri <sandhisuito@gmail.com>
2026-07-15 15:15:52 +0700

    Create registry_manager.py

---
+++ domus/registry_manager.py
@@ -0,0 +1,21 @@
+class RegistryManager:
+
+    def __init__(self):
+
+        self.registry = Registry()
+
+    def register_reference(self, reference):
+
+        ...
+
+    def register_figure(self, figure):
+
+        ...
+
+    def register_table(self, table):
+
+        ...
+
+    def get_registry(self):
+
+        return self.registry
