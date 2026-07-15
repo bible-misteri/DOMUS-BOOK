b63b1d58f644064d3bb5f184805382ece3921124
bible-misteri <sandhisuito@gmail.com>
2026-07-16 00:09:47 +0700

    Create base.py

---
+++ domus/footnote/base.py
@@ -0,0 +1,9 @@
+# ----------------------------------
+# Base Footnote
+# ----------------------------------
+
+class BaseFootnote:
+
+    def render(self, reference):
+
+        raise NotImplementedError
