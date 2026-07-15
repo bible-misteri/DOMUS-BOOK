926f570f7f0478897eb3a371d3221af512b61560
bible-misteri <sandhisuito@gmail.com>
2026-07-15 15:38:17 +0700

    Update publisher.py

--- domus/publisher.py
+++ domus/publisher.py
@@ -7,12 +7,8 @@ from domus.citation import CitationEngine
 
 class Publisher:
 
-    def __init__(self, database):
+    from domus.registry_manager import RegistryManager
 
-        self.citation = CitationEngine(database)
+    self.registry_manager = RegistryManager()
 
-    def publish(self, markdown):
-
-        result = self.citation.replace(markdown)
-
-        return result.markdown
+    self.citation = CitationEngine(database)
