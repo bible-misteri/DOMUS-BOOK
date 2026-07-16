2110765fd887fb1e1934fa9861732595b7125d2d
bible-misteri <sandhisuito@gmail.com>
2026-07-17 02:16:43 +0700

    Update bibliography.py

--- domus/plugins/builtins/bibliography.py
+++ domus/plugins/builtins/bibliography.py
@@ -1,1 +1,12 @@
+from domus.plugins import Plugin
 
+
+class BibliographyPlugin(Plugin):
+
+    name = "bibliography"
+
+    priority = 50
+
+    def process(self, context):
+
+        return context
