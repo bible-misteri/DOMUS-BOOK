9d5feddfc2e536ed7364b506f016ac01bec96516
bible-misteri <sandhisuito@gmail.com>
2026-07-16 18:12:42 +0700

    Create plugin.py

---
+++ domus/plugins/plugin.py
@@ -0,0 +1,15 @@
+# ----------------------------------
+# DOMUS Plugin
+# ----------------------------------
+
+class Plugin:
+
+    name = "plugin"
+
+    priority = 100
+
+    enabled = True
+
+    def process(self, context):
+
+        return context
