2fd12ac2010619fb8d824bdfd47ffd32b403552d
bible-misteri <sandhisuito@gmail.com>
2026-07-18 01:26:59 +0700

    Create tools.py

---
+++ domus/ai/tools.py
@@ -0,0 +1,23 @@
+class Tools:
+
+    def __init__(
+        self,
+        bible,
+        glossary,
+        bibliography,
+        crossref,
+        publisher,
+        renderer
+    ):
+
+        self.bible = bible
+
+        self.glossary = glossary
+
+        self.bibliography = bibliography
+
+        self.crossref = crossref
+
+        self.publisher = publisher
+
+        self.renderer = renderer
