ecc390f617279e1bfb6d1affcbcf63423c892924
bible-misteri <sandhisuito@gmail.com>
2026-07-18 01:47:14 +0700

    Create prompts.py

---
+++ domus/wizard/prompts.py
@@ -0,0 +1,13 @@
+# ----------------------------------
+# DOMUS Wizard Prompts
+# ----------------------------------
+
+TITLE = "Book Title"
+
+AUTHOR = "Author"
+
+LANGUAGE = "Language"
+
+THEME = "Theme"
+
+OUTPUT = "Output Format"
