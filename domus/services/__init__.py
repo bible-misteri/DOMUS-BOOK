65367bcec80c0165f5f046fedfb9bd6c97cdbb15
bible-misteri <sandhisuito@gmail.com>
2026-07-18 01:06:04 +0700

    Update __init__.py

--- domus/services/__init__.py
+++ domus/services/__init__.py
@@ -1,3 +1,7 @@
 from .glossary import GlossaryService
 from .crossref import CrossReferenceService
 from .registry import RegistryService
+from .bible import BibleService
+from .bibliography import BibliographyService
+from .publisher import PublisherService
+from .renderer import RendererService
