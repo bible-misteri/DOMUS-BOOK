ced33df1fc5a1d36d65f488d08842efd9fa906fb
bible-misteri <sandhisuito@gmail.com>
2026-07-18 01:45:57 +0700

    Create base.py

---
+++ domus/wizard/base.py
@@ -0,0 +1,13 @@
+# ----------------------------------
+# DOMUS Base Wizard
+# ----------------------------------
+
+class BaseWizard:
+
+    name = "Wizard"
+
+    version = "1.0.0"
+
+    def run(self):
+
+        raise NotImplementedError
