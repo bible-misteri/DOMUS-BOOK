035558391d78718b290a16cca22c9d53faa2ce42
bible-misteri <sandhisuito@gmail.com>
2026-07-18 13:47:10 +0700

    Create selection.py

---
+++ domus/editor/selection.py
@@ -0,0 +1,13 @@
+class Selection:
+
+    def __init__(self):
+
+        self.start = 0
+
+        self.end = 0
+
+    def set(self, start, end):
+
+        self.start = start
+
+        self.end = end
