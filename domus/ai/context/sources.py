78f1057f949e19e8b0b075d947fa0171e5c304be
bible-misteri <sandhisuito@gmail.com>
2026-07-18 20:41:57 +0700

    Create sources.py

---
+++ domus/ai/context/sources.py
@@ -0,0 +1,22 @@
+class ContextSources:
+
+    def project(self):
+        return {}
+
+    def chapter(self):
+        return {}
+
+    def glossary(self):
+        return []
+
+    def bibliography(self):
+        return []
+
+    def references(self):
+        return []
+
+    def knowledge(self):
+        return []
+
+    def memory(self):
+        return []
