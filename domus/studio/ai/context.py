3fc9174005b83a392283185e738576280579d70a
bible-misteri <sandhisuito@gmail.com>
2026-07-18 14:11:42 +0700

    Create context.py

---
+++ domus/studio/ai/context.py
@@ -0,0 +1,13 @@
+class ProjectContext:
+
+    def __init__(self):
+
+        self.project = None
+
+        self.chapter = None
+
+        self.cursor = None
+
+    def set_project(self, project):
+
+        self.project = project
