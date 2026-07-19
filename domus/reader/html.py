64e22157143fbcee44d257b1314bbb2eaa46def7
bible-misteri <sandhisuito@gmail.com>
2026-07-19 12:33:57 +0700

    Update html.py

--- domus/reader/html.py
+++ domus/reader/html.py
@@ -1,1 +1,40 @@
+from pathlib import Path
 
+
+class HTMLDocument:
+
+    def __init__(self, title):
+
+        self.title = title
+
+        self.body = []
+
+    def add(self, html):
+
+        self.body.append(html)
+
+    def render(self):
+
+        body = "\n".join(self.body)
+
+        return f"""<!DOCTYPE html>
+<html>
+
+<head>
+
+<meta charset="utf-8">
+
+<title>{self.title}</title>
+
+<link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+{body}
+
+</body>
+
+</html>
+"""
