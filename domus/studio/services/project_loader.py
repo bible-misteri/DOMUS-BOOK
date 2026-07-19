c01d00bb4cfcc93e771f4133ff015e093877b228
bible-misteri <sandhisuito@gmail.com>
2026-07-19 17:30:00 +0700

    Update project_loader.py

--- domus/studio/services/project_loader.py
+++ domus/studio/services/project_loader.py
@@ -1,5 +1,11 @@
 class ProjectService:
 
+    def load_projects(self):
+
+        """Mengembalikan daftar seluruh project."""
+
+        pass
+    
     def load_project(self, name):
 
         """Membuka satu project."""
