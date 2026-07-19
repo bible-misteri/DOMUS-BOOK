c4e4205c5cec424f9a040fba28847ced9e3b97f9
bible-misteri <sandhisuito@gmail.com>
2026-07-19 16:53:12 +0700

    Update project.py

--- domus/studio/model/project.py
+++ domus/studio/model/project.py
@@ -1,1 +1,18 @@
+from dataclasses import dataclass
+from pathlib import Path
 
+
+@dataclass
+class Project:
+
+    name: str
+
+    path: Path
+
+    author: str = ""
+
+    language: str = ""
+
+    progress: int = 0
+
+    status: str = "Draft"
