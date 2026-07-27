83f12f9fd9804ff12d02a9b401c88bb7b2b7bd8e
bible-misteri <sandhisuito@gmail.com>
2026-07-27 09:56:54 +0700

    Update books.js

--- domus/app/services/books.js
+++ domus/app/services/books.js
@@ -11,3 +11,54 @@ progress:37
 };
 
 }
+
+import {get} from "./api.js";
+
+
+async function getBooks(){
+
+return [
+
+{
+id:"DOMUS001",
+
+title:
+"DOMUS ISAACI",
+
+subtitle:
+"Membaca Kembali Kisah Yakub dan Esau",
+
+cover:
+"assets/covers/domus-isaaci-cover.jpg",
+
+progress:37,
+
+status:"Writing"
+
+}
+
+];
+
+}
+
+
+
+async function getActiveBook(){
+
+const books =
+await getBooks();
+
+
+return books[0];
+
+}
+
+
+
+export {
+
+getBooks,
+
+getActiveBook
+
+};
