e5dfd79b2538ea799888d7c65c1b377e6ad54802
bible-misteri <sandhisuito@gmail.com>
2026-07-28 00:32:33 +0700

    Create routes.js

---
+++ domus/app/config/routes.js
@@ -0,0 +1,22 @@
+import HomePage from "../pages/Home/index.js";
+import BooksPage from "../pages/Books/index.js";
+import EditorPage from "../pages/Editor/index.js";
+import ReviewPage from "../pages/Review/index.js";
+import PreviewPage from "../pages/Preview/index.js";
+import PublishPage from "../pages/Publish/index.js";
+
+export default [
+
+    { path: "home", page: HomePage },
+
+    { path: "books", page: BooksPage },
+
+    { path: "editor", page: EditorPage },
+
+    { path: "review", page: ReviewPage },
+
+    { path: "preview", page: PreviewPage },
+
+    { path: "publish", page: PublishPage }
+
+];
