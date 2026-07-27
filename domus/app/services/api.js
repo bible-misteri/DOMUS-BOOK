0d1b425481a78fb3956d3578f0c7c858e9bb0092
bible-misteri <sandhisuito@gmail.com>
2026-07-27 09:55:27 +0700

    Update api.js

--- domus/app/services/api.js
+++ domus/app/services/api.js
@@ -15,3 +15,47 @@ async post(path,data){
 }
 
 };
+
+const API_CONFIG={
+
+mode:"local",
+
+endpoint:""
+
+};
+
+
+
+async function get(path){
+
+if(API_CONFIG.mode==="local"){
+
+return localData(path);
+
+}
+
+
+const response =
+await fetch(
+API_CONFIG.endpoint+path
+);
+
+
+return response.json();
+
+}
+
+
+
+async function localData(path){
+
+
+return {};
+
+}
+
+
+
+export {
+get
+};
