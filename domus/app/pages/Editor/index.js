d681c481c775e37a1b50bef355cc8bbe398a9241
bible-misteri <sandhisuito@gmail.com>
2026-07-29 05:24:15 +0700

    Create index.js

---
+++ domus/app/pages/Editor/index.js
@@ -0,0 +1,23 @@
+import Page from "../../core/Page.js";
+
+export default class EditorPage extends Page {
+
+    async load() {
+
+        // Muat bab aktif
+
+    }
+
+    renderContent() {
+
+        return `
+
+<h1>Editor</h1>
+
+<p>Mulai menulis.</p>
+
+`;
+
+    }
+
+}
