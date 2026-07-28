7530caac507c54d398d25faeef145124659a0ac6
bible-misteri <sandhisuito@gmail.com>
2026-07-29 05:25:35 +0700

    Create index.js

---
+++ domus/app/pages/Review/index.js
@@ -0,0 +1,23 @@
+import Page from "../../core/Page.js";
+
+export default class ReviewPage extends Page {
+
+    async load() {
+
+        // Muat hasil review
+
+    }
+
+    renderContent() {
+
+        return `
+
+<h1>Review</h1>
+
+<p>Periksa naskah.</p>
+
+`;
+
+    }
+
+}
