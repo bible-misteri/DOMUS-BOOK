f6512f1f57d101756324a9835387d0d24c524370
bible-misteri <sandhisuito@gmail.com>
2026-07-29 05:17:18 +0700

    Create index.js

---
+++ domus/app/pages/Home/index.js
@@ -0,0 +1,23 @@
+import Page from "../../core/Page.js";
+
+export default class HomePage extends Page {
+
+    async load() {
+
+        // Muat data Home
+
+    }
+
+    renderContent() {
+
+        return `
+
+<h1>Home</h1>
+
+<p>Selamat datang di DOMUS.</p>
+
+`;
+
+    }
+
+}
