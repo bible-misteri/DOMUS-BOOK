287b2449ea55506b5490e5b7177c38cbfae8a9d9
bible-misteri <sandhisuito@gmail.com>
2026-07-29 04:12:30 +0700

    Create Footer.js

---
+++ domus/app/components/layout/Footer.js
@@ -0,0 +1,17 @@
+import Component from "../../core/Component.js";
+
+export default class Footer extends Component {
+
+    template() {
+
+        return `
+<footer class="domus-footer">
+
+DOMUS Framework v1.0
+
+</footer>
+`;
+
+    }
+
+}
