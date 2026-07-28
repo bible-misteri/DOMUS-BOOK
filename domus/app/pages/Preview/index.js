006b856f2bed833aaa3fd9f7cc0403580087fb55
bible-misteri <sandhisuito@gmail.com>
2026-07-29 05:26:29 +0700

    Create index.js

---
+++ domus/app/pages/Preview/index.js
@@ -0,0 +1,23 @@
+import Page from "../../core/Page.js";
+
+export default class PreviewPage extends Page {
+
+    async load() {
+
+        // Muat preview
+
+    }
+
+    renderContent() {
+
+        return `
+
+<h1>Preview</h1>
+
+<p>Pratinjau buku.</p>
+
+`;
+
+    }
+
+}
