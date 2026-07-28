6e05e950a441d66665f36a64544c8ae8173e9cd2
bible-misteri <sandhisuito@gmail.com>
2026-07-29 04:13:35 +0700

    Create Status bar.js

---
+++ domus/app/components/layout/Status bar.js
@@ -0,0 +1,24 @@
+import Component from "../../core/Component.js";
+import Store from "../../core/Store.js";
+
+export default class StatusBar extends Component {
+
+    template() {
+
+        const chapter = Store.get("activeChapter");
+
+        return `
+<div class="domus-statusbar">
+
+<span>
+
+${chapter ? chapter.title : "Belum ada bab aktif"}
+
+</span>
+
+</div>
+`;
+
+    }
+
+}
