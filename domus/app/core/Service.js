fda04af3ff3ce6adc8d94d1150cbd1c9abbe6462
bible-misteri <sandhisuito@gmail.com>
2026-07-29 03:31:15 +0700

    Update Service.js

--- domus/app/core/Service.js
+++ domus/app/core/Service.js
@@ -1,1 +1,54 @@
+/*
+====================================================
+DOMUS Framework v1.0
+Base Service
+====================================================
+*/
 
+import Logger from "./Logger.js";
+
+export default class Service {
+
+    constructor(name = "Service") {
+
+        this.name = name;
+
+    }
+
+    log(...message) {
+
+        Logger.info(`[${this.name}]`, ...message);
+
+    }
+
+    debug(...message) {
+
+        Logger.debug(`[${this.name}]`, ...message);
+
+    }
+
+    warn(...message) {
+
+        Logger.warn(`[${this.name}]`, ...message);
+
+    }
+
+    error(...message) {
+
+        Logger.error(`[${this.name}]`, ...message);
+
+    }
+
+    async initialize() {
+
+        return true;
+
+    }
+
+    async destroy() {
+
+        return true;
+
+    }
+
+}
