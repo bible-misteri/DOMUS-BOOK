53f7db9da144d4d7eaafa68da93d2e4df2e6eb21
bible-misteri <sandhisuito@gmail.com>
2026-07-29 05:21:01 +0700

    Create index.js

---
+++ domus/app/pages/Books/index.js
@@ -0,0 +1,23 @@
+import Page from "../../core/Page.js";
+
+export default class BooksPage extends Page {
+
+    async load() {
+
+        // Muat daftar buku
+
+    }
+
+    renderContent() {
+
+        return `
+
+<h1>Buku Saya</h1>
+
+<p>Daftar seluruh buku.</p>
+
+`;
+
+    }
+
+}
