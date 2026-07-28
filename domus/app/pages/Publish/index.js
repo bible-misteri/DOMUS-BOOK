af81484e834fb395891832cca9b7959682c66482
bible-misteri <sandhisuito@gmail.com>
2026-07-29 05:27:27 +0700

    Create index.js

---
+++ domus/app/pages/Publish/index.js
@@ -0,0 +1,23 @@
+import Page from "../../core/Page.js";
+
+export default class PublishPage extends Page {
+
+    async load() {
+
+        // Muat data publish
+
+    }
+
+    renderContent() {
+
+        return `
+
+<h1>Publish</h1>
+
+<p>Terbitkan buku.</p>
+
+`;
+
+    }
+
+}
