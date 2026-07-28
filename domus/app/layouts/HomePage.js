2c590a272b71487bcf47c31bea3afbf02a1b8569
bible-misteri <sandhisuito@gmail.com>
2026-07-29 04:47:16 +0700

    Create HomePage.js

---
+++ domus/app/layouts/HomePage.js
@@ -0,0 +1,18 @@
+export default class HomePage extends Page {
+
+    async load() {
+
+        // ambil data buku
+
+    }
+
+    renderContent() {
+
+        return `
+            <h1>Home</h1>
+            ...
+        `;
+
+    }
+
+}
