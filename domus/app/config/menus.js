5da8acf94ad76ea4651b7531e5ff8aeeefa85682
bible-misteri <sandhisuito@gmail.com>
2026-07-29 10:57:07 +0700

    Create menus.js

---
+++ domus/app/config/menus.js
@@ -0,0 +1,39 @@
+/*
+====================================================
+DOMUS Menu Configuration
+====================================================
+*/
+
+export default [
+
+    {
+        title: "Home",
+        page: "home"
+    },
+
+    {
+        title: "Books",
+        page: "books"
+    },
+
+    {
+        title: "Editor",
+        page: "editor"
+    },
+
+    {
+        title: "Review",
+        page: "review"
+    },
+
+    {
+        title: "Preview",
+        page: "preview"
+    },
+
+    {
+        title: "Publish",
+        page: "publish"
+    }
+
+];
