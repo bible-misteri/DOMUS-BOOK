bef99f2b3ae676060b0b5a1526715da1cbde340a
bible-misteri <sandhisuito@gmail.com>
2026-08-01 13:25:22 +0700

    Update main.js

--- domus/app/main.js
+++ domus/app/main.js
@@ -6,4 +6,10 @@ DOMUS Entry Point
 
 import Bootstrap from "./core/Bootstrap.js";
 
-Bootstrap();
+Bootstrap().catch(error => {
+
+    console.error("DOMUS gagal dijalankan:");
+
+    console.error(error);
+
+});
