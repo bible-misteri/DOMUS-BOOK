b9a77e43fd6adeac379561bdad45897c7ba64c2e
bible-misteri <sandhisuito@gmail.com>
2026-08-01 23:30:14 +0700

    Update Bootstrap.js

--- domus/app/core/Bootstrap.js
+++ domus/app/core/Bootstrap.js
@@ -5,31 +5,8 @@ DOMUS Bootstrap
 */
 
 import Renderer from "./Renderer.js";
-
-import Application from "./Application.js";
-
-import Router from "./Router.js";
-
-export default async function Bootstrap(){
-
-    Renderer.initialize("#app");
-
-    Application.registerRouter(Router);
-
-    await Application.start();
-
-}
-
-/*
-====================================================
-DOMUS Bootstrap
-====================================================
-*/
-
-import Renderer from "./Renderer.js";
 import Application from "./Application.js";
 import Router from "./Router.js";
-
 import routes from "../config/routes.js";
 
 export default async function Bootstrap() {
@@ -37,9 +14,7 @@ export default async function Bootstrap() {
     Renderer.initialize("#app");
 
     routes.forEach(route => {
-
         Router.register(route.path, route.page);
-
     });
 
     Application.registerRouter(Router);
