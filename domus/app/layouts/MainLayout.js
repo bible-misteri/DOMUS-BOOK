449645369531d0b49f1d9e66d5b78c6577ea93fa
bible-misteri <sandhisuito@gmail.com>
2026-08-10 20:18:20 +0700

    Update MainLayout.js

--- domus/app/layouts/MainLayout.js
+++ domus/app/layouts/MainLayout.js
@@ -12,6 +12,8 @@ import Sidebar from "../components/layout/Sidebar.js";
 import Footer from "../components/layout/Footer.js";
 import StatusBar from "../components/layout/StatusBar.js";
 
+import Router from "../core/Router.js";
+
 export default class MainLayout extends Layout {
 
     constructor(content = "") {
@@ -60,4 +62,22 @@ export default class MainLayout extends Layout {
 
     }
 
-}
+    afterRender() {
+
+        this.element
+            .querySelectorAll("[data-page]")
+            .forEach(button => {
+
+                button.addEventListener("click", () => {
+
+                    Router.go(
+                        button.dataset.page
+                    );
+
+                });
+
+            });
+
+    }
+
+} 
