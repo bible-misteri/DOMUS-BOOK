7dafa38782f303cbf364bd861c4f1027b418445e
bible-misteri <sandhisuito@gmail.com>
2026-08-10 17:06:23 +0700

    Update main.js

--- domus/app/main.js
+++ domus/app/main.js
@@ -1,21 +1,49 @@
+/*
+====================================================
+DOMUS Entry Point
+====================================================
+*/
+
 const app = document.querySelector("#app");
 
 function debug(msg) {
     console.log(msg);
-    app.innerHTML += `<div>${msg}</div>`;
+
+    if (app) {
+        app.innerHTML += `
+            <div style="
+                padding:6px;
+                font-family:monospace;
+            ">
+                ${msg}
+            </div>
+        `;
+    }
 }
 
 debug("main.js dimulai");
 
-fetch("./core/Bootstrap.js")
-    .then(r => {
-        debug("Status: " + r.status);
-        return r.text();
-    })
-    .then(text => {
-        debug("Panjang file: " + text.length);
-        debug(text.substring(0, 200));
+import("./core/Bootstrap.js")
+    .then(async ({ default: Bootstrap }) => {
+
+        debug("Bootstrap berhasil di-load");
+
+        await Bootstrap();
+
+        debug("Bootstrap selesai");
+
     })
     .catch(err => {
-        debug(err.toString());
+
+        debug("ERROR:");
+        debug(err.message);
+
+        if (err.stack) {
+            debug(
+                err.stack.replace(/\n/g, "<br>")
+            );
+        }
+
+        console.error(err);
+
     });
